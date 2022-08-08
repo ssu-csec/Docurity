64ba4a51f6e88a713ca5dd7bfbc5bf18b268f6ff
kordood <gigacms@gmail.com>
2022-08-08 16:05:16 +0900

    run enc dec script

---
+++ scripts/run_dec.sh
@@ -0,0 +1,3 @@
+./insert_initial.sh ../cbc_bench_dec tmp output_cbc_dec_ 10
+./insert_initial.sh ../ctr_bench_dec tmp output_ctr_dec_ 10
+./insert_initial.sh ../dl_ecb_bench_dec tmp output_dl_ecb_dec_ 10
