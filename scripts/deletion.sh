40c8069a76a369c3f5f5ba5923607d0bf7a81a77
kordood <gigacms@gmail.com>
2022-08-08 17:05:57 +0900

    if break

--- scripts/deletion.sh
+++ scripts/deletion.sh
@@ -2,7 +2,7 @@ Binary=$1
 Tmpfile=$2
 Output=$3
 MAX_SIZE=$4
-Constant=$5
+Constant=${5:-0}
 Threshold=$((12+$Constant))
 Operation="Delete"
 Inversed_index=$(($MAX_SIZE-$Threshold))
@@ -10,7 +10,7 @@ echo "$0 ${Binary} ${Tmpfile} ${MAX_SIZE}"
 
 rm $Tmpfile
 
-./string_printer ${MAX_SIZE}) >> $Tmpfile
+./string_printer ${MAX_SIZE} >> $Tmpfile
 for ((i=1; i<$MAX_SIZE+1; i++))
 do
 	echo "${Binary} ${i}" 
@@ -19,6 +19,8 @@ do
 	echo "$Threshold" >> $Tmpfile
 	Threshold=$(($Threshold * 2))
 	Inversed_index=$(($Inversed_index-$Threshold))
+
+	if [ $Inversed_index -lt 0 ]; then break; fi
 done
 
 ./$Binary $Tmpfile> $Output
