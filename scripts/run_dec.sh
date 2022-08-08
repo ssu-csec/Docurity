7cb824ff0229699585fa9c775a985df8147ac7e1
kordood <gigacms@gmail.com>
2022-08-08 16:05:58 +0900

    relative

--- scripts/run_dec.sh
+++ scripts/run_dec.sh
@@ -1,3 +1,3 @@
-./insert_initial.sh ../cbc_bench_dec tmp output_cbc_dec_ 10
-./insert_initial.sh ../ctr_bench_dec tmp output_ctr_dec_ 10
-./insert_initial.sh ../dl_ecb_bench_dec tmp output_dl_ecb_dec_ 10
+./insert_initial.sh ../cbc_bench_dec tmp output_cbc_dec_ $1
+./insert_initial.sh ../ctr_bench_dec tmp output_ctr_dec_ $1
+./insert_initial.sh ../dl_ecb_bench_dec tmp output_dl_ecb_dec_ $1
