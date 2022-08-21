5be191e07647ec0c95f1c3905e55171b632917e5
Byeori-Kim <99kbr11@gmail.com>
2022-08-21 03:43:36 -0700

    Add test code and test code generator

--- test_file_generator.c
+++ test_file_generator.c
@@ -4,9 +4,9 @@
 
 #define buf_size 1024
 
-#define index 20
+#define index 172
 
-#define iteration 40
+#define iteration 20
 
 int main (int argc, char **argv)
 {
@@ -28,18 +28,32 @@ int main (int argc, char **argv)
 	}
 	
     char file_name[25];
-    if(size/1024 == 0)
+    if(size/1024 == 0)                                                          // for modifying 1kb
     {
-        sprintf(file_name, "test_%s_%s.txt", argv[1], argv[2]);
+        sprintf(file_name, "test_%s_%s_1kb.txt", argv[1], argv[2]);
     }
     else if(size/(1024*1024) == 0)
     {
-        sprintf(file_name, "test_%dkb_%s.txt", size/1024, argv[2]);
+        sprintf(file_name, "test_%dkb_%s_1kb.txt", size/1024, argv[2]);
     }
     else
     {
-        sprintf(file_name, "test_%dmb_%s.txt", size/(1024*1024), argv[2]);
+        sprintf(file_name, "test_%dmb_%s_1kb.txt", size/(1024*1024), argv[2]);
     }
+
+    // if(size/1024 == 0)                                                       // for inserting 1kb aand increasing or deleting 1/100 of initial data size
+    // {
+    //     sprintf(file_name, "test_%s_%s.txt", argv[1], argv[2]);
+    // }
+    // else if(size/(1024*1024) == 0)
+    // {
+    //     sprintf(file_name, "test_%dkb_%s.txt", size/1024, argv[2]);
+    // }
+    // else
+    // {
+    //     sprintf(file_name, "test_%dmb_%s.txt", size/(1024*1024), argv[2]);
+    // }
+
     FILE *input_file = fopen(file_name, "w");
     if (input_file == NULL) {
         fputs("File error", stderr);
@@ -56,7 +70,13 @@ int main (int argc, char **argv)
     {
         for(int i = 0; i < iteration; i++)
         {
-            fprintf(input_file, "Insert\n%d\n%s\n", index, insert_data);
+            fprintf(input_file, "Insert\n%d\n%s", index, insert_data);
+            // for(int j = 0; j < i; j ++)                                      // for increasing inserted data size 
+            // {
+            //     fprintf(input_file, "%s", insert_data);
+            // }
+            fprintf(input_file, "\n");
+
         }
         
     }
@@ -64,8 +84,12 @@ int main (int argc, char **argv)
     {
         for(int i = 0; i < iteration; i++)
         {
-            fprintf(input_file, "Delete\n%d\n%d\n", index, size/100);
+            fprintf(input_file, "Delete\n%d\n%d\n", index, buf_size);        // for deleting 1kb
         }
+        // for(int i = 0; i < iteration; i++)
+        // {
+        //     fprintf(input_file, "Delete\n%d\n%d\n", index, size/100);           // for deleting 1/100 of initial data size
+        // }
     }
 
 
